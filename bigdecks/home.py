4354a9c7c89766968a86df4a562d074e50353939
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-02-17 10:34:44 -0500

    feat: Add home page

---
+++ bigdecks/home.py
@@ -0,0 +1,18 @@
+"""Home
+
+This module contains the logic for the home page.
+"""
+
+
+from flask import (
+    Blueprint, render_template
+)
+
+# Create the blueprint for the index page at /index
+bp = Blueprint(name="home", import_name="__name__")
+
+
+# Set up the route to home.
+@bp.route("/")
+def home():
+    return render_template("home.html")
