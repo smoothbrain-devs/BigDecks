531a3c17b2d063187e838ece0ede0682ee80f8e2
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-04-14 12:19:49 -0400

    feat(cards.py): swap to using card_service

--- bigdecks/cards.py
+++ bigdecks/cards.py
@@ -1,11 +1,11 @@
 """Cards enpoint management"""
 
 
-from .models.card import Card
 from flask import (
     Blueprint,
     render_template,
 )
+from .services.card_service import CardService
 
 
 bp = Blueprint(name="cards", import_name=__name__)
@@ -13,17 +13,11 @@ bp = Blueprint(name="cards", import_name=__name__)
 
 @bp.route("/card")
 def home():
-    card = Card.get_random_card()
+    card = CardService.get_random_card()
     return render_template("cards.html", card=card)
 
 
 @bp.route("/card/<scry_id>")
 def card_by_scry_id(scry_id: str):
-    card = Card.get_card_by_scryfall_id(scry_id)
-    return render_template("cards.html", card=card)
-
-
-@bp.route("/card/<set_name>/<collector_number>/<card_name>")
-def card_by_set_num_name(set_name: str, collector_number: str, card_name: str):
-    card = Card.get_card(set_name, collector_number, card_name)
+    card = CardService.get_card_by_scryfall_id(scry_id)
     return render_template("cards.html", card=card)
